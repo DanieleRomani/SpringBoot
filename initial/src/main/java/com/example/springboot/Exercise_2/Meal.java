12d16e33bad81d5899f4e7d61320d30ff46b27ab
DanieleRomani <124621366+DanieleRomani@users.noreply.github.com>
2023-06-06 16:54:48 +0200

    Exercise_2

---
+++ initial/src/main/java/com/example/springboot/Exercise_2/Meal.java
@@ -0,0 +1,53 @@
+package com.example.springboot.Exercise_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Meal {
+    private String nameMeal;
+    private String descriptionMeal;
+    private double priceMeal;
+    private static List<Meal> meals;
+
+    public Meal(String nameMeal, String descriptionMeal, double priceMeal) {
+        this.nameMeal = nameMeal;
+        this.descriptionMeal = descriptionMeal;
+        this.priceMeal = priceMeal;
+    }
+
+    public String getNameMeal() {
+        return nameMeal;
+    }
+
+    public void setNameMeal(String nameMeal) {
+        this.nameMeal = nameMeal;
+    }
+
+    public String getDescriptionMeal() {
+        return descriptionMeal;
+    }
+
+    public void setDescriptionMeal(String descriptionMeal) {
+        this.descriptionMeal = descriptionMeal;
+    }
+
+    public double getPriceMeal() {
+        return priceMeal;
+    }
+
+    public void setPriceMeal(double priceMeal) {
+        this.priceMeal = priceMeal;
+    }
+
+    public static List<Meal> getMeals() {
+        return meals;
+    }
+
+    public static void mealList() {
+        meals = new ArrayList<>();
+        meals.add(new Meal("Spaghetti alla carbonara", "spaghetti, uova, pancetta, pecorino", 12.00));
+        meals.add(new Meal("Pizza Margherita", "Pomodoro, Mozzarella", 8.00));
+        meals.add(new Meal("Insalata di pollo", "insalata, maionese, pollo", 15.00));
+    }
+}
+
---
+++ initial/src/main/java/com/example/springboot/Exercise_2/PathVariables.java
@@ -0,0 +1,87 @@
+package com.example.springboot.Exercise_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PathVariables {
+    /**
+     * Exercise 1: Create a GetMapping that returns a list of meals
+     * <p>
+     * 1 - Annotate a new class with the @RestController annotation.
+     * 2 - Create a new endpoint "/meals" using the @GetMapping annotation.
+     * 3 - In the method, return a list of Meal objects.
+     */
+
+    @GetMapping("/meals")
+    public List<Meal> getMeals() {
+        Meal.mealList();
+        return Meal.getMeals();
+    }
+
+    /**
+     * Exercise 2: Create a GetMapping that returns a meal by name
+     *
+     * 1 - Annotate a new class with the @RestController annotation.
+     * 2 - Create a new endpoint "/meal/{name}" using the @GetMapping annotation.
+     * 3 - In the method, add a query parameter "name" using the @PathVariable annotation.
+     * 4 - Return a Meal object with the corresponding name.
+     */
+
+    @GetMapping("/meal/{name}") //* Sostituisci -> /{name} con -> "/Spagetti alla carbonara" o un'altra pietanza che trovi nella calasse Meal.
+    public Meal getMealByName(@PathVariable("name") String name) {
+        Meal.mealList();
+        for (Meal meal : Meal.getMeals()) {
+            if (meal.getNameMeal().equalsIgnoreCase(name)) {  //* equalsIgnoreCase = ingora differenze di carattere
+                return meal;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Exercise 3: Create a GetMapping that returns a meal by any word of description
+     *
+     * 1 - Annotate a new class with the @RestController annotation.
+     * 2 - Create a new endpoint "/meal/description-match/{phrase}" using the @GetMapping annotation.
+     * 3 - In the method, add a query parameter "description" using the @PathVariable annotation.
+     * 4 - Return a Meal object with the corresponding description.
+     */
+
+    @GetMapping("/meal/description-match/{phrase}") //* Sostituisci -> /{pharse} con -> "/pollo" o un'altra descrizione che trovi nella calasse Meal.
+    public Meal getMealByDescription(@PathVariable("phrase") String phrase) {
+        Meal.mealList();
+        for (Meal meal : Meal.getMeals()) {
+            if (meal.getDescriptionMeal().toLowerCase().contains(phrase.toLowerCase())) {
+                return meal;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Exercise 4: Create a GetMapping that returns a meal by price range
+     *
+     * 1 - Annotate a new class with the @RestController annotation.
+     * 2 - Create a new endpoint "/meal/price" using the @GetMapping annotation.
+     * 3 - In the method, add two query parameters "min" and "max" using the @RequestParam annotation.
+     * 4 - Return a list of Meal objects with prices within the specified range.
+     */
+
+    @GetMapping("/meal/price") //* Inserire in Postman -> /meal/price?min=10.00&max=20.00
+    public List<Meal> getMealsByPriceRange(@RequestParam("min") double minPrice, @RequestParam("max") double maxPrice) {
+        Meal.mealList();
+        List<Meal> mealsInRange = new ArrayList<>();
+        for (Meal meal : Meal.getMeals()) {
+            if (meal.getPriceMeal() >= minPrice && meal.getPriceMeal() <= maxPrice) {
+                mealsInRange.add(meal);
+            }
+        }
+        return mealsInRange;
+    }
+}
